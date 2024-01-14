666ca482dbd49c431af62f53d6ca8bf0ead9cf13
Ibnballo1 <belloabdullateef035@gmail.com>
2024-01-14 22:01:33 +0100

    Update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "Current directory: $(pwd)"
-ls -l ./bin
-echo "Executing './bin/rails server'"
-
 bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
